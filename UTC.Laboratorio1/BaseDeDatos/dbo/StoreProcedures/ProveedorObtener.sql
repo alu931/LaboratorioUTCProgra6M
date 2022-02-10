a8495e43973ad5502fa8a262e5317d94d602497a
alu931 <alu931@gmail.com>
2022-02-09 20:28:24 -0600

    Proyecto de base de datos Entidad Proveedor

---
+++ UTC.Laboratorio1/BaseDeDatos/dbo/StoreProcedures/ProveedorObtener.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[ProveedorObtener]
+	@IdProveedor INT = NULL
+
+AS 
+BEGIN
+  SET NOCOUNT ON
+
+  /*Obtener datos de proveedor*/
+  SELECT
+         IdProveedor
+		,Identificacion
+		,Nombre
+		,PrimerApellido
+		,SegundoApellido
+		,Edad
+		,FechaNacimiento
+  FROM 
+       dbo.Proveedor
+  WHERE
+      (@IdProveedor IS NULL OR IdProveedor=@IdProveedor)
+
+END
--- UTC.Laboratorio1/BaseDeDatos/dbo/Tables/SolicitudExterna.sql
+++
@@ -1,4 +0,0 @@
-﻿CREATE TABLE [dbo].[SolicitudExterna]
-(
-	[Id] INT NOT NULL PRIMARY KEY
-)
