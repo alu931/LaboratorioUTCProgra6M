f4971784db5011cfcec9986327eaa5833b85298f
alu931 <alu931@gmail.com>
2022-02-23 20:59:43 -0600

    Clase 5

---
+++ UTC.Laboratorio1/WebApp/wwwroot/App/Proveedor/Grid.js
@@ -0,0 +1,18 @@
+"use strict";
+var ProveedorGrid;
+(function (ProveedorGrid) {
+    if (MensajeApp != "") {
+        Toast.fire({ icon: "success", title: MensajeApp });
+    }
+    function OnClickEliminar(id) {
+        ComfirmAlert("¿Desea eliminar el registro seleccionado?", "Eliminar", "warning", '#3085d6', '#d33')
+            .then(function (result) {
+            if (result.isConfirmed) {
+                window.location.href = "Proveedor/Grid?handler=Eliminar&id=" + id;
+            }
+        });
+    }
+    ProveedorGrid.OnClickEliminar = OnClickEliminar;
+    $("#GridView").DataTable();
+})(ProveedorGrid || (ProveedorGrid = {}));
+//# sourceMappingURL=Grid.js.map
---
+++ UTC.Laboratorio1/WebApp/wwwroot/App/Proveedor/Grid.ts
@@ -0,0 +1,27 @@
+﻿namespace ProveedorGrid {
+
+    declare var MensajeApp;
+
+    if (MensajeApp != "") {
+        Toast.fire({ icon: "success", title: MensajeApp });
+       }
+
+
+    export function OnClickEliminar(id) {
+
+        ComfirmAlert("¿Desea eliminar el registro seleccionado?", "Eliminar", "warning", '#3085d6', '#d33')
+            .then(result => {
+                if (result.isConfirmed) {
+                    window.location.href = "Proveedor/Grid?handler=Eliminar&id=" + id;
+
+                }
+
+
+            });
+    }
+
+
+    $("#GridView").DataTable();
+
+
+}
