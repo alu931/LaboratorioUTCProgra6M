2bc0aaeb3cfd4db8174e5a500a6c5e1b9a8bdb90
alu931 <alu931@gmail.com>
2022-03-16 20:24:09 -0600

    Clase 6

--- UTC.Laboratorio1/WebApp/Pages/Proveedor/Edit.cshtml.cs
+++ UTC.Laboratorio1/WebApp/Pages/Proveedor/Edit.cshtml.cs
@@ -4,13 +4,86 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WBL;
+using Entity;
 
 namespace WebApp.Pages.Proveedor
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly IProveedorService proveedor;
+
+        public EditModel(IProveedorService proveedor)
         {
+            this.proveedor = proveedor;
         }
+
+        [BindProperty(SupportsGet = true)]
+        public int? id { get; set; }
+
+        [BindProperty]
+        public ProveedorEntity Entity { get; set; } = new ProveedorEntity();
+
+
+        public async Task<IActionResult> OnGet()
+        {
+            try
+            {
+                if (id.HasValue)
+                {
+                    Entity = await proveedor.GetById(new() {
+                    IdProveedor=id
+                    });
+                }
+
+                return Page();
+
+            }
+            catch (Exception ex)
+            {
+
+                return Content(ex.Message);
+            }       
+        
+        
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            try
+            {
+                //Metodo de Actualización
+                if (Entity.IdProveedor.HasValue)
+                {
+                    var result = await proveedor.Update(Entity);
+
+                    if (result.CodeError != 0) throw new Exception(result.MsgError);
+                    TempData["Msg"] = "El registro se actualizó correctamente";
+
+                }
+                else
+                {   //Metodo de Inserción
+                    var result = await proveedor.Create(Entity);
+
+                    if (result.CodeError != 0) throw new Exception(result.MsgError);
+                    TempData["Msg"] = "El registro se agregró correctamente";
+                }
+
+                return RedirectToPage("Grid");
+            }
+            catch (Exception ex)
+            {
+
+                return Content(ex.Message);
+            }
+
+
+        }
+
+
+
+
+
+
     }
 }
--- UTC.Laboratorio1/WebApp/wwwroot/App/Proveedor/Edit.ts
+++ UTC.Laboratorio1/WebApp/wwwroot/App/Proveedor/Edit.ts
@@ -1,1 +1,25 @@
-﻿
+﻿namespace ProveedorEdit {
+
+    var Formulario = new Vue(
+        {
+            data:
+            {
+                Formulario: "#FormEdit"
+            },
+            mounted()
+            {
+                CreateValidator(this.Formulario);
+            }
+
+        }
+    );
+
+    Formulario.$mount("#AppEdit");
+
+
+
+
+
+
+
+}
